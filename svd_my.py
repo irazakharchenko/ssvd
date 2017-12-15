b6361de640279baaadcda4e8d4e50a9104bbd38a
irazakharchenko <zakharchenko@ucu.edu.ua>
2017-12-15 18:48:39 +0200

    fixed bugs,  better deshuffle, it works

--- svd_my.py
+++ svd_my.py
@@ -6,6 +6,7 @@ from random import randint
 from skimage import color, io
 from scipy._lib.six import xrange
 
+
 def sorterForEigenValuesAndVectors(val, vect):
     """
     vect need to be transpose
@@ -53,30 +54,32 @@ def svd(a, r):
     S = np.array([math_sqrt(x) for x in eig_val])
     E = makeDiagonalFromValues(S, np.shape(VT)[0], min_len)
     U = make_U(S, A, eig_vect, min_len)
-    #print("U", np.shape(U), "\nE", np.shape(E), "\nVT", np.shape(VT))
+    # print("U", np.shape(U), "\nE", np.shape(E), "\nVT", np.shape(VT))
     UE = np.dot(U, E)
     UEVT = np.dot(UE, VT)
-    #print("A", A, "\nmaybe A", np.real(UEVT))
-    #pl.imshow(A, cmap=cm.Greys_r)
-    #pl.show()
-    #pl.imshow(np.real(UEVT), cmap=cm.Greys_r)
-    #pl.show()
+    # print("A", A, "\nmaybe A", np.real(UEVT))
+    # pl.imshow(A, cmap=cm.Greys_r)
+    # pl.show()
+    # pl.imshow(np.real(UEVT), cmap=cm.Greys_r)
+    # pl.show()
+
     return np.real(UEVT)
 
 
 def blockshaped(matrix):
     row_len = len(matrix[0])
     col_len = len(matrix)
-    n = round((row_len*col_len)**(1.0/4))
-    #print('n', n)
+
+    n = 16
+    # print('n', n)
     if row_len % n != 0:
-        matrix = matrix[:,:-(row_len % n) ]
+        matrix = matrix[:, :-(row_len % n)]
     if col_len % n != 0:
-        matrix = matrix[:col_len-(col_len % n)]
+        matrix = matrix[:col_len - (col_len % n)]
     nrows = row_len // n
     ncols = col_len // n
     h, w = matrix.shape
-    return (matrix.reshape(h//nrows, nrows, -1, ncols).swapaxes(1,2).reshape(-1, nrows, ncols)), n
+    return (matrix.reshape(h // nrows, nrows, -1, ncols).swapaxes(1, 2).reshape(-1, nrows, ncols)), n
 
 
 def shuffle(arr):
@@ -84,60 +87,68 @@ def shuffle(arr):
     row_len = len(blocks)
     col_len = len(blocks[0]) * len(blocks[0][0])
     must_fill = np.zeros((row_len, col_len))
-    #print("must fill ",must_fill)
+    # print("must fill ",must_fill)
 
     for i in xrange(must_fill.shape[0]):
         join = np.resize(blocks[i], (1, col_len))[0]
-        must_fill[i,:] = join
-    return must_fill,n
+        must_fill[i, :] = join
+    print('Shuffled')
+    return must_fill, n
 
 
 def ssvd(a, r):
-
-    # X[i//n + n(i % n)][j//n + n(j % n)]
+    row_len, col_len = len(a[0]), len(a)
+    if row_len != col_len:
+        min_ = min(col_len, row_len)
+        col_len = min_
+        row_len = min_
+        a = a[:min_, :min_]
     X, n = shuffle(a)
 
-    #print("X", X)
+    # print("X", X)
     svd_r = svd(X, r)
-
-    #print(svd_r, np.shape(svd_r), len(a), len(a[0]))
-    #print(n)
-    #shape = np.shape(svd_r)
-    shape =  np.shape(svd_r)
+    print('Calculated svd')
+    shape = np.shape(svd_r)
     shape_im = (len(a), len(a[0]))
-    n_in_col, n_in_row = shape_im[0] //n, shape_im[1] // n
+    block_size = shape_im[1] // n
+    block_in_row = shape_im[1] // block_size
+    shape_im = tuple([x - x % n for x in shape_im])
     As = np.zeros(shape_im)
-    #print(n*n_in_col)
-    #print(shape[0])
-    for i in range():
-        #print(i)
-        div_mod = (i // n_in_row, i % n_in_row)
-        #print((div_mod[0]*n, (div_mod[0] + 1)*n, div_mod[1]*n,(div_mod[1] + 1)*n)) # 529
-        As[div_mod[0]*n: (div_mod[0] + 1)*n, div_mod[1]*n:(div_mod[1] + 1)*n] = np.resize(svd_r[i], (n, n))
-
+    # print(shape_im)
+    for i in range(len(svd_r)):
+        div_mod = (i // block_in_row, i % block_in_row)
+        As[div_mod[0] * block_size: (div_mod[0] + 1) * block_size,
+        div_mod[1] * block_size:(div_mod[1] + 1) * block_size] = \
+            np.resize(svd_r[i], (block_size, block_size))
+    print('Reshuffle')
 
     return As
+
 
 def compareSvds(matrix, rank):
     mat = svd(matrix, rank)
     print('Calculated svd')
     mats = ssvd(matrix, rank)
+
     print('Calculated ssvd')
     pl.imshow(mat, cmap=cm.Greys_r)
-    pl.savefig('my_Lenna.jpg')
+    # pl.savefig('my_Lenna.jpg')
     pl.show()
 
     pl.imshow(mats, cmap=cm.Greys_r)
     pl.show()
 
 
-img = color.rgb2gray(io.imread('foto/Lenna.jpg'))
+img = color.rgb2gray(io.imread('foto/d.jpg'))
 pl.imshow(img, cmap=cm.Greys_r)
-#pl.savefig('grey_Lenna.jpg')
+# pl.savefig('grey_Lenna.jpg')
 # l = []
 # for i in range(randint(4,20)):
 #     st = []
 #     for j in range(randint(5,20)):
 #         st.append(randint(1, 240))
 #     l.append()
-compareSvds(img, 20)
+# compareSvds(img, 20)
+# print(l)
+# (shuffle(l))
+compareSvds(img, 14)
